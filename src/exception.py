c52ca509087fee7f08c2c0625b15ddcd96209238
Soham <sohamdeshpande242@gmail.com>
2024-06-18 23:53:31 +0530

    exception bug fix

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -20,5 +20,4 @@ class CustomException(Exception):
         return self.error_message
     
 
-
-        
+       
